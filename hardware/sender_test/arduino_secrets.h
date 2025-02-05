76f6ca6eeb24b043ca8beaaf60757651eb4db85c
Zer02z2 <zongzechan@gmail.com>
2025-02-04 23:29:53 -0500

    init

---
+++ hardware/sender_test/arduino_secrets.h
@@ -0,0 +1,16 @@
+#define SECRET_SSID "sandbox370"
+#define SECRET_PASS "+s0a+s03!2gether?"
+
+#define MQTT_USER "zongze"
+#define MQTT_PASS "zongze22"
+
+#define ENCODER_PIN1 2
+#define ENCODER_INT1 digitalPinToInterrupt(ENCODER_PIN1)
+#define ENCODER_PIN2 3
+#define ENCODER_INT2 digitalPinToInterrupt(ENCODER_PIN2)
+#define ENCODER_PIN3 9
+#define ENCODER_INT3 digitalPinToInterrupt(ENCODER_PIN3)
+#define ENCODER_PIN4 10
+#define ENCODER_INT4 digitalPinToInterrupt(ENCODER_PIN4)
+
+#define BUTTON_PIN 4
