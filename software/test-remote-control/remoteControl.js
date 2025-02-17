8451563aab7c4435a8096351f9f3c6b85cbd63dc
Zer02z2 <zongzechan@gmail.com>
2025-02-17 14:57:11 -0500

    vite init

--- software/test-remote-control/remoteControl.js
+++ software/test-remote-control/remoteControl.js
@@ -1,14 +1,11 @@
-shiftPercentage = 0
-targetShiftPercentage = 0
-globalTranslate = {
-  x: 0,
-  y: 0,
-  targetX: 0,
-  targetY: 0,
-}
-globalOpacity = {
-  now: 1,
-  target: 1,
+const globalVariables = {
+  shiftPercentage: { current: 0, target: 0 },
+  distort: {
+    xCurrent: 1,
+    xTarget: 1,
+    yCurrent: 1,
+    yTarget: 1,
+  },
 }
 
 const random = (low, high) => {
@@ -26,13 +23,30 @@ const elementData = elements.map((element) => {
   const range = 50
   const translateX = Math.floor(random(-range, range))
   const translateY = Math.floor(random(-range, range))
-  //element.style.transform = `translateX(${translateX}) translateY(${translateY})`
+  const danceScale = element === document.body ? 0 : random(0.001, 0.05)
+  const danceProgress = random(0, 2 * Math.PI)
+
+  const children = element.querySelectorAll(":scope > *")
+  const isEndChild = Array.from(children).length === 0 ? true : false
   return {
     target: element,
-    scale: scale,
-    newScale: newScale,
-    translateX: translateX,
-    translateY: translateY,
+    translateData: {
+      scale: scale,
+      newScale: newScale,
+      translateX: translateX,
+      translateY: translateY,
+    },
+    danceData: {
+      scale: danceScale,
+      progress: danceProgress,
+    },
+    isEndChild: isEndChild,
+    distortData: {
+      xNow: 1,
+      xTarget: 1,
+      yNow: 1,
+      yTarget: 1,
+    },
   }
 })
 
@@ -45,44 +59,48 @@ socket.addEventListener("message", (event) => {
   //console.log(event.data)
   const [name, data] = event.data.split(":")
   if (name == "potentiometer") {
-    if (data < 10) targetShiftPercentage = 0
-    else targetShiftPercentage = data / (1024 - 10)
-    //console.log(targetShiftPercentage)
-  }
-  const moveDistance = 500
-  if (name == "encoderX") {
-    // if (data == 1) globalTranslate.targetX += moveDistance
-    // else if (data == 0) globalTranslate.targetX -= moveDistance
-    if (data == 1) globalOpacity.target += 0.1
-    else if (data == 0) globalOpacity.target -= 0.1
-    if (globalOpacity.target <= 0) globalOpacity.target = 0
-    else if (globalOpacity.target >= 1) globalOpacity.target = 1
-  }
-  if (name == "encoderY") {
-    if (data == 1) {
-      globalTranslate.targetY += moveDistance
-    } else if (data == 0) globalTranslate.targetY -= moveDistance
+    globalVariables.shiftPercentage.target = data < 10 ? 0 : data / 1024
+  } else if (name == "encoderY") {
+    if (data === 0) {
+      globalVariables.distort.yTarget -= 0.1
+    } else if (data == 1) {
+      globalVariables.distort.yTarget += 0.1
+    }
   }
 })
 
 const animate = () => {
   window.requestAnimationFrame(animate)
-  shiftPercentage += (targetShiftPercentage - shiftPercentage) * 0.1
-  globalOpacity.now += (globalOpacity.target - globalOpacity.now) * 0.1
-  elementData.forEach((data) => {
-    const { target, scale, newScale, translateX, translateY } = data
-    const scaleNow =
-      (scale + (newScale - scale) * shiftPercentage) * globalOpacity.now
-    const xNow = `${translateX * shiftPercentage}px`
-    const yNow = `${translateY * shiftPercentage}px`
-    target.style.scale = scaleNow
-    target.style.transform = `translateX(${xNow}) translateY(${yNow})`
-    target.style.opacity = `${globalOpacity.now}`
-  })
+  const { shiftPercentage, distort } = globalVariables
+  shiftPercentage.current +=
+    (shiftPercentage.target - shiftPercentage.current) * 0.1
+  const { current } = shiftPercentage
 
-  globalTranslate.x += (globalTranslate.targetX - globalTranslate.x) * 0.1
-  globalTranslate.y += (globalTranslate.targetY - globalTranslate.y) * 0.1
-  document.body.style.transform = `translateX(${globalTranslate.x}px) translateY(${globalTranslate.y}px)`
+  distort.xCurrent += (distort.xTarget - distort.xCurrent) * 0.1
+  distort.yCurrent += (distort.yTarget - distort.yCurrent) * 0.1
+
+  elementData.forEach((data) => {
+    const { target, translateData } = data
+    const { scale, newScale, translateX, translateY } = translateData
+    const danceScale = dance(data)
+    const scaleNow = scale + (newScale - scale) * current
+    const xNow = `${translateX * current}px`
+    const yNow = `${translateY * current}px`
+
+    const distortX = data.isEndChild ? distort.xCurrent : 1
+    const distortY = data.isEndChild ? distort.yCurrent : 1
+
+    target.style.scale = scaleNow
+    target.style.transform = `translateX(${xNow}) translateY(${yNow}) scale(${distortX}, ${distortY})`
+  })
+}
+
+const dance = (data) => {
+  const danceData = data.danceData
+  danceData.progress += 0.01
+  const percentage = Math.sin(danceData.progress)
+  const danceScale = danceData.scale * percentage
+  return danceScale
 }
 
 animate()
